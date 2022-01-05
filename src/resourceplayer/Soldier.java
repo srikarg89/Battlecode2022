f9db834069d31e858c06fe33b87a60d4a13c6fae
Saahith Janapati <itzsaahith01@gmail.com>
2022-01-05 16:28:12 -0500

    add other bot classes to soldier priorities

--- src/resourceplayer/Soldier.java
+++ src/resourceplayer/Soldier.java
@@ -6,8 +6,27 @@ import battlecode.common.*;
 public class Soldier extends Robot {
     MapLocation archonLoc = null;
 
+    //list defining what we should destroy first
+    RobotType[] priorityOrder = {RobotType.ARCHON, RobotType.SOLDIER, RobotType.MINER,
+            RobotType.BUILDER, RobotType.LABORATORY, RobotType.WATCHTOWER, RobotType.SAGE};
+
     public Soldier(RobotController rc) {
         super(rc);
+    }
+
+
+    // helper method used that finds position of enemy robot type in predefined array
+    // to help us figure out which bot to destroy first
+    public int getArrayIndex(RobotType[] arr,RobotType value) {
+        int k=0;
+        for(int i=0;i<arr.length;i++){
+
+            if(arr[i]==value){
+                k=i;
+                break;
+            }
+        }
+        return k;
     }
 
     public void run() throws GameActionException {
@@ -35,31 +54,34 @@ public class Soldier extends Robot {
         RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
 
         MapLocation toAttack = null;
+        int oppTypeIndex = 10;
         RobotType oppType = null;
         int distance = Integer.MAX_VALUE;
 
+
         for (RobotInfo info : enemies) {
+            int currOppTypeIndex = getArrayIndex(priorityOrder, info.type);
+
             if (oppType == null) {
                 toAttack = info.location;
+                oppTypeIndex = currOppTypeIndex;
                 oppType = info.type;
                 distance = myLoc.distanceSquaredTo(info.location);
             }
 
-            // switch if we prioritize current robot type more
-            //TODO: need to add all the other types of robots as well
-            else if (info.type == RobotType.ARCHON && oppType == RobotType.SOLDIER
-                    || (info.type == RobotType.SOLDIER && oppType == RobotType.BUILDER)) {
+            // switch if we prioritize current opp. robot type more
+            else if (currOppTypeIndex < oppTypeIndex) {
                 toAttack = info.location;
                 oppType = info.type;
+                oppTypeIndex = currOppTypeIndex;
                 distance = myLoc.distanceSquaredTo(info.location);
             }
 
             // if same type as robot already found, only switch if we're closer
-            else if (info.type == oppType) {
+            else if (currOppTypeIndex == oppTypeIndex) {
                 int currDistance = myLoc.distanceSquaredTo(info.location);
                 if (currDistance < distance) {
                     toAttack = info.location;
-                    oppType = info.type;
                     distance = currDistance;
                 }
             }
